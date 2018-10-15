70e7da6b0708187039503a441e1d27c52ca2e17d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-15 14:48:05 +0100

    installed shotgun

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'shotgun'
+get '/' do
+  "Hello World!"
+end
+
+get '/index' do
+  "index"
+end
+
+get '/hamster' do
+  "This is a hamster"
+end
+
+get '/tea' do
+  "I don't like tea"
+end
+
+get '/fencing' do
+  "En Garde!"
+end
